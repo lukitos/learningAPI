53320c2bf1d95c59c34d010a38121dadca304cd4
Shu Sia Lukito <lukitos@yahoo.com>
2017-07-01 11:03:33 -0700

    Add CRUD for person and tag

--- routes/personRoutes.js
+++ routes/personRoutes.js
@@ -10,15 +10,60 @@ router.get('/', function(req, res, next) {
 });
 
 /* ADD one person */
-// router.post('/', function (req, res, next) {
-//   res.render('index');
-  // knex('person').insert({name: req.body.name}).then(function (person) {
-  //   knex.select().table('person').then(function(person) {
-  //     res.send(person);
-  //   });
-  // });
-// });
+router.post('/', function (req, res, next) {
+  knex('person')
+  .insert({
+      lname: req.body.lname,
+      fname: req.body.fname,
+      mname: req.body.mname,
+      email: req.body.email,
+      phone: req.body.phone,
+      twitter: req.body.twitter,
+      linkedin: req.body.linkedin,
+      github: req.body.github
+    })
+  .then(function () {
+    knex.select().table('person').then(function(person) {
+      res.send(person);
+    });
+  });
+});
 
 /* GET one person */
+router.get('/:pid', function (req, res, next) {
+  knex('person').where('id', req.params.pid).then(function (person) {
+    res.send(person);
+  });
+});
+
+/* EDIT one person */
+router.put('/:pid', function (req, res, next) {
+  knex('person')
+    .where('id', req.params.pid)
+    .update({
+      lname: req.body.lname,
+      fname: req.body.fname,
+      mname: req.body.mname,
+      email: req.body.email,
+      phone: req.body.phone,
+      twitter: req.body.twitter,
+      linkedin: req.body.linkedin,
+      github: req.body.github
+      })
+    .then(function () {
+      knex.select().table('person').then(function(person) {
+        res.send(person);
+      });
+    });
+});
+
+/* DELETE one person */
+router.delete('/:pid', function (req, res, next) {
+  knex('person').where('id', req.params.pid).del().then(function () {
+    knex.select().table('person').then(function(person) {
+      res.send(person);
+    });
+  });
+});
 
 module.exports = router;
--- routes/tagRoutes.js
+++ routes/tagRoutes.js
@@ -11,30 +11,7 @@ router.get('/', function(req, res, next) {
 
 /* ADD one tag */
 router.post('/', function (req, res, next) {
-  knex('tag').insert({name: req.body.name}).then(function (tags) {
-    knex.select().table('tag').then(function(tags) {
-      res.send(tags);
-    });
-  });
-});
-
-/* EDIT one tag */
-// router.put('/edit/:tag_id', function (req, res, next) {
-  // knex('tag').where('id', req.params.tag_id).update({name: req.body.name}).then(function (req, res, next) {
-  //   knex.select().table('tag').then(function(tags) {
-  //     res.send(tags);
-  //   });
-  // });
-// });
-
-/* EDIT one tag - STILL NOT WORKING FIX IT!!! */
-router.put('/edit/:tag_id', function (req, res, next) {
-  res.send('edit');
-});
-
-/* DELETE tag */
-router.delete('/delete/:tag_id', function (req, res, next) {
-  knex('tag').where('id', req.params.tag_id).del().then(function () {
+  knex('tag').insert({name: req.body.name}).then(function () {
     knex.select().table('tag').then(function(tags) {
       res.send(tags);
     });
@@ -42,9 +19,29 @@ router.delete('/delete/:tag_id', function (req, res, next) {
 });
 
 /* GET one tag */
-router.get('/:tag_id', function(req, res, next) {
-  knex('tag').where('id', req.params.tag_id).then(function(tags) {
+router.get('/:tid', function(req, res, next) {
+  knex('tag').where('id', req.params.tid).then(function(tags) {
     res.send(tags);
+  });
+});
+
+/* EDIT one tag */
+router.put('/:tid', function (req, res, next) {
+  knex('tag').where('id', req.params.tid)
+    .update({name: req.body.name})
+    .then(function () {
+      knex.select().table('tag').then(function(tags) {
+      res.send(tags);
+    });
+  });
+});
+
+/* DELETE tag */
+router.delete('/:tid', function (req, res, next) {
+  knex('tag').where('id', req.params.tid).del().then(function () {
+    knex.select().table('tag').then(function(tags) {
+      res.send(tags);
+    });
   });
 });
 
